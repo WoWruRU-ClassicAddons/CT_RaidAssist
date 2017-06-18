fcaa4944139ed26689558fe0cdf6c89c02273d25
M2us <Dead_Maus@mail.ru>
2017-06-18 20:49:11 +0300

    Added support ruRU locale

---
+++ CT_RaidAssist/localization.ru.lua
@@ -0,0 +1,147 @@
+if ( GetLocale() == "ruRU" ) then
+
+-- Version : Russian ( by Maus )
+
+CT_RAMENU_HOME = "Спасибо за использование CT_RaidAssist! Ниже вы можете найти различные вариации настроек. Если вы используете его в первый раз, просмотрите страницу настроек, чтобы подстроить его под свой лад. Более опытные пользователи могут воспользоваться другими страницами настроек.\n\nДля дополнительной информации, используйте |c00FFFFFF/rahelp|r.";
+CT_RAMENU_OPTIONSETS = "Вы можете сохранить свои настройки, чтобы в следующий раз использовать их по одному щелчку мыши.";
+CT_RAMENU_BOSSMODS = "Босс мод - моды, которые могут помочь вам в особых событиях. Они помогают обеспечить предупреждение о важных событиях, уведомления которые помогут вам выжить. У некоторых модов есть собственный набор настроек, так что не забудьте изменить их в соответствии с вашими потребностями.";
+
+CT_RAMENU_BUFFSDESCRIPT = "Выберите баффы и дебаффы которые бы вы хотели отобразить. Максимум можно отобразить 4 баффа. Дебаффы будут иметь другой цвет окна.";
+CT_RAMENU_BUFFSTOOLTIP = "Используйте стрелки для перемещения баффов.";
+CT_RAMENU_DEBUFFSTOOLTIP = "Используйте стрелки для перемещения дебаффов";
+CT_RAMENU_GENERALDESCRIPT = "Ниже вы найдете настройки отображения. Рейд лидер и его помощники могут воспользоваться кнопкой \"Update Status\" для обновления здоровья, маны и баффов всех участников рейда.";
+CT_RAMENU_REPORTDESCRIPT = "Заставляет вас сообщать здоровье и ману для выбранного персонажа Если вы или отмеченный персонаж покидаете группу, вы перестаете автоматически отправлять отчеты.";
+CT_RAMENU_ADDITIONALEMTOOLTIP = "Дополнительный монитор отображает здоровье до 5 членов вашей группы или рейда с низким процентом здоровья. Вы можете сами установить порог здоровья, после которого все у кого здоровье ниже указанного порога будут отображены.";
+CT_RAMENU_DEBUFFCURING = "Функция снятия проклятия позволяет настроить систему CT_RaidAssist's снятия проклятия. Это очень гибкая система и это окно позволяет настроить ее, чтобы сделать управление еще более простым.";
+CT_RAMENU_MANACONSERVE = "Установив значение на %s, позволит отключить проверку для этого заклинания.";
+
+BINDING_HEADER_CT_RAIDASSIST = "CT_RaidAssist";
+BINDING_NAME_CT_CUREDEBUFF = "Снятие рейд дебаффов"; 
+BINDING_NAME_CT_RECASTRAIDBUFF = "Обновление рейд баффов";
+BINDING_NAME_CT_SHOWHIDE = "Показать/Скрыть рейд окна";
+BINDING_NAME_CT_RESMON = "Вкл/Выкл монитор воскрешения";
+BINDING_NAME_CT_EMERGENCYMONITOR = "Показать/Скрыть дополнительный монитор";
+BINDING_NAME_CT_TOGGLEDEBUFFS = "Вкл/Выкл отображение баффов/дебаффов";
+BINDING_NAME_CT_ASSISTMT1 = "Помощь МТ 1";
+BINDING_NAME_CT_ASSISTMT2 = "Помощь MT 2";
+BINDING_NAME_CT_ASSISTMT3 = "Помощь MT 3";
+BINDING_NAME_CT_ASSISTMT4 = "Помощь MT 4";
+BINDING_NAME_CT_ASSISTMT5 = "Помощь MT 5";
+BINDING_NAME_CT_TARGETMT1 = "Цель MT 1";
+BINDING_NAME_CT_TARGETMT2 = "Цель MT 2";
+BINDING_NAME_CT_TARGETMT3 = "Цель MT 3";
+BINDING_NAME_CT_TARGETMT4 = "Цель MT 4";
+BINDING_NAME_CT_TARGETMT5 = "Цель MT 5";
+BINDING_NAME_CT_ASSISTPT1 = "Помощь PT 1";
+BINDING_NAME_CT_ASSISTPT2 = "Помощь PT 2";
+BINDING_NAME_CT_ASSISTPT3 = "Помощь PT 3";
+BINDING_NAME_CT_ASSISTPT4 = "Помощь PT 4";
+BINDING_NAME_CT_ASSISTPT5 = "Помощь PT 5";
+BINDING_NAME_CT_TARGETPT1 = "Цель PT 1";
+BINDING_NAME_CT_TARGETPT2 = "Цель PT 2";
+BINDING_NAME_CT_TARGETPT3 = "Цель PT 3";
+BINDING_NAME_CT_TARGETPT4 = "Цель PT 4";
+BINDING_NAME_CT_TARGETPT5 = "Цель PT 5";
+BINDING_NAME_CT_TOGGLESORTTYPE = "Вкл/Выкл тип сортировки";
+BINDING_NAME_CT_TOGGLESORTTYPE2 = "Вкл/Выкл сортировку группы/класса";
+
+CT_RAMENU_VISITSITE = "Для получения дополнительной информации, пожалуйста, посетите наш сайт: http://www.ctmod.net";
+
+-- Spells
+
+CT_RA_POWERWORDFORTITUDE = { "Слово силы: Стойкость", "Молитва стойкости" };
+CT_RA_MARKOFTHEWILD = { "Знак дикой природы", "Дар дикой природы" };
+CT_RA_ARCANEINTELLECT = { "Чародейский интеллект", "Чародейская гениальность" };
+CT_RA_ADMIRALSHAT = "Адмиральская шляпа";
+CT_RA_POWERWORDSHIELD = "Слово силы: Щит";
+CT_RA_SOULSTONERESURRECTION = "Воскрешение камнем души";
+CT_RA_DIVINESPIRIT = { "Божественный дух", "Молитва духа" };
+CT_RA_THORNS = "Шипы";
+CT_RA_FEARWARD = "Защита от страха";
+CT_RA_SHADOWPROTECTION = { "Защита от темной магии", "Молитва защиты от темной магии" };
+CT_RA_BLESSINGOFMIGHT = { "Благословение могущества", "Великое благословение могущества" };
+CT_RA_BLESSINGOFWISDOM = { "Благословение мудрости", "Великое благословение мудрости" };
+CT_RA_BLESSINGOFKINGS = { "Благословение королей", "Великое благословение королей" };
+CT_RA_BLESSINGOFSALVATION = { "Благословение спасения", "Великое благословение спасения" };
+CT_RA_BLESSINGOFLIGHT = { "Благословение Света", "Великое благословение Света" };
+CT_RA_BLESSINGOFSANCTUARY = { "Благословение неприкосновенности", "Великое благословение неприкосновенности" };
+CT_RA_RENEW = "Обновление";
+CT_RA_REGROWTH = "Восстановление";
+CT_RA_REJUVENATION = "Омоложение";
+CT_RA_FIRESHIELD = "Огненный щит"
+CT_RA_DAMPENMAGIC = "Ослабление магии";
+CT_RA_AMPLIFYMAGIC = "Усиление магии";
+
+CT_RA_WARRIOR = "Воин";
+CT_RA_ROGUE = "Разбойник";
+CT_RA_HUNTER = "Охотник";
+CT_RA_MAGE = "Маг";
+CT_RA_WARLOCK = "Чернокнижник";
+CT_RA_DRUID = "Друид";
+CT_RA_PRIEST = "Жрец";
+CT_RA_SHAMAN = "Шаман";
+CT_RA_PALADIN = "Паладин";
+CT_RA_PETS = "Питомцы";
+
+CT_RA_AFKMESSAGE = "Вы отошли от компьютера, оставив сообщение: \"(.+)\"%.";
+CT_RA_DNDMESSAGE = "Вы заняты. Сообщение: \"(.+)\"%.";
+
+CT_RA_MAGIC = "Магия";
+CT_RA_DISEASE = "Болезнь";
+CT_RA_POISON = "Яд";
+CT_RA_CURSE = "Проклятие";
+CT_RA_WEAKENEDSOUL = "Ослабленная душа";
+CT_RA_RECENTLYBANDAGED = "Недавняя перевязка";
+CT_RA_MINDVISION = "Внутреннее зрение";
+CT_RA_DREAMLESSSLEEP = "Мирный сон";
+CT_RA_GREATERDREAMLESSSLEEP = "Глубокий сон без сновидений";
+
+-- Cures
+CT_RA_DISPELMAGIC = "Рассеивание заклинаний";
+CT_RA_ABOLISHDISEASE = "Устранение болезни";
+CT_RA_ABOLISHPOISON = "Устранение яда";
+CT_RA_CLEANSE = "Очищение";
+CT_RA_PURIFY = "Омовение";
+CT_RA_REMOVECURSE = "Снятие проклятия";
+CT_RA_CUREDISEASE = "Излечение болезни";
+CT_RA_CUREPOISON = "Выведение яда";
+CT_RA_REMOVELESSERCURSE = "Снятие малого проклятия";
+
+-- Resurrections
+CT_RA_RESURRECTION = "Воскрешение";
+CT_RA_REDEMPTION = "Искупление";
+CT_RA_REBIRTH = "Возрождение";
+CT_RA_ANCESTRALSPIRIT = "Дух предков";
+CT_RA_REINCARNATION = "Перерождение";
+
+-- Pets
+CT_RA_IMP = "Бес";
+CT_RA_VOIDWALKER = "Демон Бездны";
+CT_RA_SUCCUBUS = "Суккуб";
+CT_RA_FELHUNTER = "Охотник Скверны";
+CT_RA_DOOMGUARD = "Стражник ужаса";
+CT_RA_INFERNAL = "Инфернал";
+
+-- RAReg/RADur
+CT_RA_DURABILITY = "^Прочность%: (%d+) / (%d+)$";
+CT_REG_PRIEST = "Священная свеча";
+CT_REG_PRIEST_SPELL = "Молитва стойкости";
+CT_REG_MAGE = "Порошок чар";
+CT_REG_MAGE_SPELL = "Чародейская гениальность";
+CT_REG_DRUID = "Дикий шипокорень";
+CT_REG_DRUID_SPELL = "Дар дикой природы";
+CT_REG_WARLOCK = "Осколок души";
+CT_REG_SHAMAN = "Крест";
+CT_REG_SHAMAN_SPELL = "Перерождение";
+CT_REG_PALADIN = "Знак божественности";
+CT_REG_PALADIN_SPELL = "Божественное вмешательство";
+
+-- Hunter's Mark
+CT_RA_HUNTERSMARK = "Метка охотника";
+
+-- "Regexps"
+CT_RA_HASJOINEDRAIDREGEXP = "^([^%s]+) присоединяется к рейдовой группе%.$";
+CT_RA_HASLEFTRAIDREGEXP = "^([^%s]+) покидает рейдовую группу%.$";
+CT_RA_TANKHASDIEDREGEXP = "^([^%s]+) погибает%.$";
+end
+CT_RA_FEIGNDEATH = { ["ru"] = "Притвориться мертвым" };
